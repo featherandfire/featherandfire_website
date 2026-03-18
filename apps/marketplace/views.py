8df06a1fb0a05491f0dd33d60fa614b4f6ae8a6d
Hotheadheather <hkwhittlesey@hotmail.com>
2026-03-18 14:58:05 -0500

    Fix hero image rotator to use json_script instead of inline Django tags in JS

--- apps/marketplace/views.py
+++ apps/marketplace/views.py
@@ -7,11 +7,15 @@ def home(request):
     featured_artists = ArtistProfile.objects.filter(is_featured=True).select_related('user')[:3]
     artwork_count = Artwork.objects.count()
     artist_count = ArtistProfile.objects.count()
+    hero_artworks = Artwork.objects.filter(is_sold=False).exclude(image='').order_by('?')[:12]
+    hero_artwork_urls = [a.image.url for a in hero_artworks]
 
     context = {
         'featured_artists': featured_artists,
         'artwork_count': artwork_count,
         'artist_count': artist_count,
+        'hero_artworks': hero_artworks,
+        'hero_artwork_urls': hero_artwork_urls,
     }
     return render(request, 'home.html', context)
 
